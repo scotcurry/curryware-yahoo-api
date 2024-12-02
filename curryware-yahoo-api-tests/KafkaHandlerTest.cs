a282e8383c2ce12f56dbc8258f8b04a26bc01807
Scot Curry <scotcurry4@gmail.com>
2024-12-02 13:28:21 -0500

    Forgot to comment out Kafka Tests.

--- curryware-yahoo-api-tests/KafkaHandlerTest.cs
+++ curryware-yahoo-api-tests/KafkaHandlerTest.cs
@@ -6,21 +6,21 @@ namespace curryware_yahoo_api_tests;
 
 public class KafkaHandlerTest
 {
-    [Fact]
-    public void KafkaHandlerListTopicsTest()
-    {
-        var kafkaAdmin = new KafkaAdmin();
-        var topicsList = kafkaAdmin.GetTopics();
-        Assert.True(topicsList.Count > 0);
-    }
-    
-    [Fact]
-    public async Task CreatePlayerMessageTest()
-    {
-        var topicName = "PlayerTopic";
-        var dateString = DateTime.Now.ToString( "MM/dd/yyyy hh:mm:ss tt");
-        var returnValue = await PlayerProducer.SendPlayerData(topicName, "Scot" + dateString);
-        Assert.True(returnValue);
-    }
+    // [Fact]
+    // public void KafkaHandlerListTopicsTest()
+    // {
+    //     var kafkaAdmin = new KafkaAdmin();
+    //     var topicsList = kafkaAdmin.GetTopics();
+    //     Assert.True(topicsList.Count > 0);
+    // }
+    //
+    // [Fact]
+    // public async Task CreatePlayerMessageTest()
+    // {
+    //     var topicName = "PlayerTopic";
+    //     var dateString = DateTime.Now.ToString( "MM/dd/yyyy hh:mm:ss tt");
+    //     var returnValue = await PlayerProducer.SendPlayerData(topicName, "Scot" + dateString);
+    //     Assert.True(returnValue);
+    // }
 } 
     
