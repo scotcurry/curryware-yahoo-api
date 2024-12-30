e96b3b32691fc5806218b2f4eb96eec1625dd38f
Scot Curry <scotcurry4@gmail.com>
2024-12-30 17:28:20 -0500

    Update PostgresLibrary.cs - Saved password

--- curryware-postgres-library/PostgresLibrary.cs
+++ curryware-postgres-library/PostgresLibrary.cs
@@ -12,7 +12,7 @@ public class PostgresLibrary
             connectionStringBuilder.Host = "ubuntu-postgres.curryware.org";
             connectionStringBuilder.Port = 5432;
             connectionStringBuilder.Username = "scot";
-            connectionStringBuilder.Password = "AirWatch1";
+            connectionStringBuilder.Password = "****";
             connectionStringBuilder.Database = "currywarefantasy";
             string connectionString = connectionStringBuilder.ConnectionString;
             
