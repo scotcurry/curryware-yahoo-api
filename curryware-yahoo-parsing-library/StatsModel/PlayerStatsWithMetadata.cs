40da855a4e3417df28754c7bace1db2becd79596
Scot Curry <scotcurry4@gmail.com>
2025-02-13 19:52:37 -0500

    Merging changes.

--- curryware-yahoo-parsing-library/StatsModel/PlayerStatsWithMetadata.cs
+++
@@ -1,7 +0,0 @@
-namespace curryware_yahoo_parsing_library.StatsModel;
-
-public class PlayerStatsWithMetadata
-{
-    public string? OAuthToken { get; set; }
-    public List<PlayerStats>? PlayerStats { get; set; }
-}
